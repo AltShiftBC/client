d8511a181ab0967b08585ac1cfd6b1ecb2686ec9
Gisele-123 <giselemigisha53@gmail.com>
2024-07-25 02:21:26 -0700

    control panel icon changed

--- fishot_ui/src/components/sidebar.jsx
+++ fishot_ui/src/components/sidebar.jsx
@@ -9,16 +9,18 @@ import settings from "../assets/icons/Settings.png"
 import video from "../assets/icons/video.png"
 import { Icon } from "@iconify/react/dist/iconify.js";
 
+
+
 const Sidebar = () => {
     return (
         <div className="sidebar">
-            <div className="one">
+            <div className="one" >
                 <img src={logo} alt="" />
                 <p id="fishot">Fishot</p>
             </div>
             <div className="icons">
                 <div id="home"><Icon icon="ic:outline-home" style={{fontSize: '30px'}}/><p>Home</p></div>
-                <div id="control"><img src={control} alt="" /><p>Control panel</p></div>
+                <div id="control"><Icon icon="carbon:ibm-z-os-ai-control-interface" style={{fontSize: '30px'}}/><p>Control panel</p></div>
                 <div id="video"><Icon icon="carbon:video-filled" style={{fontSize: '30px'}}/><p>Video feed</p></div>
                 <div id="remote"><Icon icon="mdi:remote"style={{fontSize: '30px'}}/><p>Remote control</p></div>
                 <div id="analytics"><Icon icon="tabler:device-analytics" style={{fontSize: '30px'}}/><p>Analytics</p></div>
