10ffc6f4e2815260d1ae3e8cf28bd280aa15fad1
Gisele-123 <giselemigisha53@gmail.com>
2024-08-04 01:43:04 -0700

    Added emergency on remote control

--- fishot_ui/src/components/RemoteControl.jsx
+++ fishot_ui/src/components/RemoteControl.jsx
@@ -1,30 +1,45 @@
 import React, { useState, useRef, useEffect } from "react";
 import { Icon } from "@iconify/react/dist/iconify.js";
-import video from "../assets/video-feed.png";
-import feed1 from "../assets/icons/feed1.svg";
-import feed2 from "../assets/icons/feed2.svg";
-import feed3 from "../assets/icons/feed3.svg";
-import feed4 from "../assets/icons/feed4.svg";
-// import { Icon } from "@iconify/react";
 import '../styles/RemoteControl.css';
 import '../styles/video.css';
 
 const RemoteControl = () => {
-    
+    const statusData = [
+        {
+            icon: "mdi:internet",
+            desc: "Status",
+            value: "Online",
+            iconStyle: {},
+            containerStyle: {}
+        },
+        {
+            icon: "iconoir:trash-solid",
+            desc: "Weight",
+            value: "7.6kg",
+            iconStyle: { color: "#411900" },
+            containerStyle: { backgroundColor: "#A4540021" }
+        },
+        {
+            icon: "ic:baseline-mode-standby",
+            desc: "Mode",
+            value: "Auto",
+            iconStyle: { color: "#4E60FF" },
+            containerStyle: { backgroundColor: "#4E60FF4D" }
+        }
+    ];
     const [isPlaying, setIsPlaying] = useState(false);
     const [currentTime, setCurrentTime] = useState(0);
     const [recordingStartTime, setRecordingStartTime] = useState(null);
-    // const [elapsedTime, setElapsedTime] = useState(0);
+    const [elapsedTime, setElapsedTime] = useState(0);
     const [mediaRecorder, setMediaRecorder] = useState(null);
     const [recordedChunks, setRecordedChunks] = useState([]);
     const videoRef = useRef(null);
-
     useEffect(() => {
         const startCamera = async () => {
             try {
                 const stream = await navigator.mediaDevices.getUserMedia({ video: true });
                 videoRef.current.srcObject = stream;
-                
+
                 const recorder = new MediaRecorder(stream);
                 setMediaRecorder(recorder);
 
@@ -38,10 +53,10 @@ const RemoteControl = () => {
                     setRecordingStartTime(Date.now());
                 };
 
-                // recorder.onstop = () => {
-                //     setRecordingStartTime(null);
-                //     setElapsedTime(0);
-                // };
+                recorder.onstop = () => {
+                    setRecordingStartTime(null);
+                    setElapsedTime(0);
+                };
             } catch (error) {
                 console.error("Error accessing camera: ", error);
             }
@@ -55,7 +70,6 @@ const RemoteControl = () => {
             }
         };
     }, []);
-
     useEffect(() => {
         let interval;
         if (isPlaying && recordingStartTime) {
@@ -67,7 +81,6 @@ const RemoteControl = () => {
         }
         return () => clearInterval(interval);
     }, [isPlaying, recordingStartTime]);
-
     const handlePlayPause = () => {
         if (isPlaying) {
             videoRef.current.pause();
@@ -78,17 +91,14 @@ const RemoteControl = () => {
         }
         setIsPlaying(!isPlaying);
     };
-
     const handleRewind = () => {
         videoRef.current.currentTime -= 10;
         setCurrentTime(videoRef.current.currentTime);
     };
-
     const handleForward = () => {
         videoRef.current.currentTime += 10;
         setCurrentTime(videoRef.current.currentTime);
     };
-
     const trimVideo = (chunks, trimTime) => {
         return new Promise((resolve, reject) => {
             const blob = new Blob(chunks, { type: 'video/webm' });
@@ -130,7 +140,6 @@ const RemoteControl = () => {
             };
         });
     };
-
     const handleDownload = () => {
         const blob = new Blob(recordedChunks, { type: 'video/webm' });
         const url = URL.createObjectURL(blob);
@@ -140,34 +149,25 @@ const RemoteControl = () => {
         link.click();
         URL.revokeObjectURL(url);
     };
-
     const [isToggled, setIsToggled] = useState({ open: false, close: false });
 
     const handleToggle = (type) => {
         setIsToggled((prev) => ({ ...prev, [type]: !prev[type] }));
     };
-
     const formatTime = (seconds) => {
         const minutes = Math.floor(seconds / 60);
         const secs = seconds % 60;
         return `${minutes}:${secs.toString().padStart(2, '0')}`;
     };
     return (
-        <div className="remote-control"> 
-            <div className="content">
+        <div className="content">
             <div className="video-control">
-                <video 
-                    ref={videoRef} 
-                    width="900" 
-                    autoPlay 
+                <video
+                    ref={videoRef}
+                    width="950"
+                    autoPlay
                     onTimeUpdate={() => setCurrentTime(videoRef.current.currentTime)}
                 />
-                <div>
-                    <img src={feed1} alt="feed1" id="feed1" />
-                    <img src={feed2} alt="feed2" id="feed2" />
-                    <img src={feed3} alt="feed3" id="feed3" />
-                    <img src={feed4} alt="feed4" id="feed4" />
-                </div>
                 <div className="video-live">
                     <p>REC</p>
                     <div className="record-on"></div>
@@ -178,61 +178,65 @@ const RemoteControl = () => {
                         <p>Live</p>
                     </div>
                     <div className="control-icons">
-                        <Icon 
-                            id="video-icon" 
-                            icon="material-symbols:fast-rewind-outline" 
-                            style={{ cursor: "pointer" }} 
-                            onClick={handleRewind} 
+                        <Icon
+                            id="video-icon"
+                            icon="material-symbols:fast-rewind-outline"
+                            style={{ cursor: "pointer" }}
+                            onClick={handleRewind}
                         />
                         {isPlaying ? (
-                            <Icon 
-                                id="video-icon" 
-                                icon="zondicons:pause-outline" 
-                                style={{ cursor: "pointer" }} 
-                                onClick={handlePlayPause} 
+                            <Icon
+                                id="video-icon"
+                                icon="zondicons:pause-outline"
+                                style={{ cursor: "pointer" }}
+                                onClick={handlePlayPause}
                             />
                         ) : (
-                            <Icon 
-                                id="video-icon" 
-                                icon="octicon:play-16" 
-                                style={{ cursor: "pointer" }} 
-                                onClick={handlePlayPause} 
+                            <Icon
+                                id="video-icon"
+                                icon="octicon:play-16"
+                                style={{ cursor: "pointer" }}
+                                onClick={handlePlayPause}
                             />
                         )}
-                        <Icon 
-                            id="video-icon" 
-                            icon="material-symbols:fast-forward-outline" 
-                            style={{ cursor: "pointer" }} 
-                            onClick={handleForward} 
+                        <Icon
+                            id="video-icon"
+                            icon="material-symbols:fast-forward-outline"
+                            style={{ cursor: "pointer" }}
+                            onClick={handleForward}
                         />
                     </div>
                     <div className="download-icon">
-                        <Icon 
-                            id="video-icon" 
-                            icon="material-symbols:file-download" 
-                            style={{ cursor: "pointer" }} 
-                            onClick={handleDownload} 
+                        <Icon
+                            id="video-icon"
+                            icon="material-symbols:file-download"
+                            style={{ cursor: "pointer" }}
+                            onClick={handleDownload}
                         />
                     </div>
                 </div>
-                </div>
-                <div className="status-div">
-                <div id="status-container">
-                        <div className="status-icon"><Icon icon="mdi:internet" id="
-                    icon"/></div>
-                        <div id="status-desc"><p style={{ fontWeight: "bold" }}>Status</p><p style={{ color: "#818181" }}>Online</p></div>
-                    </div>
-                    <div id="status-container">
-                        <div className="status-icon" style={{ backgroundColor: "#A4540021" }}><Icon icon="iconoir:trash-solid" style={{ color: "#411900" }} id="
-                    icon"/></div>
-                        <div id="status-desc"><p style={{ fontWeight: "bold" }}>Weight</p><p style={{ color: "#818181" }}>7.6kg</p></div>
-                    </div>
-                    <div id="status-container">
-                        <div className="status-icon" style={{ backgroundColor: "#4E60FF4D" }}><Icon icon="ic:baseline-mode-standby" style={{ color: "#4E60FF" }} id="
-                    icon"/></div>
-                        <div id="status-desc"><p style={{ fontWeight: "bold" }}>Mode</p><p style={{ color: "#818181" }}>Auto</p></div>
-                    </div>
             </div>
+            <div className="status-div">
+                {statusData.map((item, index) => (
+                    <div key={index} id="status-container" style={{background:"#fff"}}>
+                        <div className="status-icon" style={item.containerStyle}>
+                            <Icon icon={item.icon} id="icon" style={item.iconStyle}/>
+                        </div>
+                        <div id="status-desc">
+                            <p style={{ fontWeight: "bold" }}>{item.desc}</p>
+                            <p style={{ color: "#818181" }}>{item.value}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <div className="emergency">
+                <Icon id="icon-arrow-back" icon="ep:arrow-left-bold" style={{ color: "#fff", cursor: "pointer" }} />
+                <button>Emergency</button>
+                <div className="emergency-icons">
+                    <Icon id="icon" icon="mingcute:wifi-fill" style={{ color: "#fff", cursor: "pointer" }} />
+                    <Icon id="icon" icon="gg:battery" style={{ color: "#04D700", cursor: "pointer" }} />
+                    <Icon id="icon" icon="twemoji:compass" style={{ cursor: "pointer" }} />
+                </div>
             </div>
         </div>
     );
--- fishot_ui/src/components/homeTop.jsx
+++ fishot_ui/src/components/homeTop.jsx
@@ -7,60 +7,81 @@ import graph from "../assets/graph.png"
 import { Link } from "react-router-dom"
 
 const HomeTop = () => {
+    const statusData = [
+        {
+            icon: "mdi:internet",
+            desc: "Status",
+            value: "Online",
+            iconStyle: {},
+            containerStyle: {}
+        },
+        {
+            icon: "gg:battery",
+            desc: "Battery",
+            value: "78%",
+            iconStyle: { color: "#FF4848" },
+            containerStyle: { backgroundColor: "#FF6F6F21" }
+        },
+        {
+            icon: "iconoir:trash-solid",
+            desc: "Weight",
+            value: "7.6kg",
+            iconStyle: { color: "#411900" },
+            containerStyle: { backgroundColor: "#A4540021" }
+        },
+        {
+            icon: "ic:baseline-mode-standby",
+            desc: "Mode",
+            value: "Auto",
+            iconStyle: { color: "#4E60FF" },
+            containerStyle: { backgroundColor: "#4E60FF4D" }
+        }
+    ];
     return (
         <div className="hometop-div">
             <div className="sideOne">
                 <div className="status-div">
-                    <div id="status-container">
-                        <div className="status-icon"><Icon icon="mdi:internet" id="
-                    icon"/></div>
-                        <div id="status-desc"><p style={{ fontWeight: "bold" }}>Status</p><p style={{ color: "#818181" }}>Online</p></div>
-                    </div>
-                    <div id="status-container">
-                        <div className="status-icon" style={{ backgroundColor: "#FF6F6F21" }}><Icon icon="gg:battery" style={{ color: "#FF4848" }} id="
-                    icon"/></div>
-                        <div id="status-desc"><p style={{ fontWeight: "bold" }}>Battery</p><p style={{ color: "#818181" }}>78%</p></div>
-                    </div>
-                    <div id="status-container">
-                        <div className="status-icon" style={{ backgroundColor: "#A4540021" }}><Icon icon="iconoir:trash-solid" style={{ color: "#411900" }} id="
-                    icon"/></div>
-                        <div id="status-desc"><p style={{ fontWeight: "bold" }}>Weight</p><p style={{ color: "#818181" }}>7.6kg</p></div>
-                    </div>
-                    <div id="status-container">
-                        <div className="status-icon" style={{ backgroundColor: "#4E60FF4D" }}><Icon icon="ic:baseline-mode-standby" style={{ color: "#4E60FF" }} id="
-                    icon"/></div>
-                        <div id="status-desc"><p style={{ fontWeight: "bold" }}>Mode</p><p style={{ color: "#818181" }}>Auto</p></div>
-                    </div>
+                    {statusData.map((item, index) => (
+                        <div key={index} id="status-container" style={{ background: "#fff" }}>
+                            <div className="status-icon" style={item.containerStyle}>
+                                <Icon icon={item.icon} id="icon" style={item.iconStyle} />
+                            </div>
+                            <div id="status-desc">
+                                <p style={{ fontWeight: "bold" }}>{item.desc}</p>
+                                <p style={{ color: "#818181" }}>{item.value}</p>
+                            </div>
+                        </div>
+                    ))}
                 </div>
                 <div className="map">
-                    <img src={map} alt=""/>
+                    <img src={map} alt="" />
                 </div>
             </div>
             <div className="sideTwo">
                 <div className="profile-div">
                     <div className="profile-pic-id">
                         <img src={profile} alt="" />
-                        <p style={{ textAlign: "center" }}> <span style={{fontWeight:"bold"}}>John Doe</span> <br />
+                        <p style={{ textAlign: "center" }}> <span style={{ fontWeight: "bold" }}>John Doe</span> <br />
                             Welcome back!</p>
                     </div>
                     <div className="profile-set">
-                     <Link to="/settings"><Icon icon="material-symbols:manage-accounts-outline" style={{cursor:"pointer"}}/></Link>
-                        <Link to="/logout"><Icon icon="material-symbols:logout" style={{cursor:"pointer"}}/></Link>
+                        <Link to="/settings"><Icon icon="material-symbols:manage-accounts-outline" style={{ cursor: "pointer" }} /></Link>
+                        <Link to="/logout"><Icon icon="material-symbols:logout" style={{ cursor: "pointer" }} /></Link>
                     </div>
                 </div>
                 <div className="statistic-div">
-                    <div className="statistic-name"><p style={{fontWeight:"bold"}}>Depth</p><p style={{color:"#9F9F9F"}}>Sea level</p></div>
+                    <div className="statistic-name"><p style={{ fontWeight: "bold" }}>Depth</p><p style={{ color: "#9F9F9F" }}>Sea level</p></div>
                     <div className="statistic-graph">
                         <div id="statistic-label">
-                            <p style={{fontSize:"13px"}}>
-                              0 <br />
-                            100 <br />
-                            200 <br />
-                            300
+                            <p style={{ fontSize: "13px" }}>
+                                0 <br />
+                                100 <br />
+                                200 <br />
+                                300
                             </p>
                         </div>
                         <div id="statictic-main-graph">
-                            <img src={graph} alt="" height={90} width={180}/>
+                            <img src={graph} alt="" height={90} width={180} />
                         </div>
                     </div>
                 </div>
